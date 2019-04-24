094035e725eef1e499c7d2eda54bb028bc63f5b9
aYIfseec <754912298@qq.com>
2019-04-25 07:24:16 +0800

    引入了多余的sun.jvm.hotspot.utilities.Assert导致无法打包

--- src/main/java/com/hyk/shoppingstreet/controller/CartController.java
+++ src/main/java/com/hyk/shoppingstreet/controller/CartController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import sun.jvm.hotspot.utilities.Assert;
 
 @RestController
 @RequestMapping(value = "/cart")
